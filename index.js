26398f15095d7ceeee233b786ba72ef4c8699634
bathulapraka <bathulaprakash406@gmail.com>
2023-12-07 15:42:59 +0530

    local storage

---
+++ index.js
@@ -0,0 +1,22 @@
+
+localStorage.setItem('class 1' ,'this is first time to visiting a flipkart');
+
+
+localStorage.setItem('class2' ,'this is my first time storing a local storage');
+
+
+
+
+localStorage.setItem('class3', 'mini statement');
+
+
+
+localStorage.getItem('class 4 ', 'nothing to say about local storage is good');
+
+
+
+localStorage.getItem('class 1', 'nothing to say about local storage is good');
+
+
+
+localStorage.setItem('class2' , 'replacing the actual value');
